32e5c291ba9209ca317ca7c371bfea97d31806b4
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-23 13:34:47 -0500

    6 - Using Python Decouple and Dj Database Url

--- src/cfehome/db.py
+++ src/cfehome/db.py
@@ -1,12 +1,13 @@
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
+from cfehome.env import config
+import dj_database_url
 
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': 'codingforentrepreneurs',
-    'PASSWORD': '87nJysegjYAc',
-    'HOST': 'ep-late-bonus-361115.us-east-2.aws.neon.tech',
-    'PORT': '5432',
-  }
+DATABASE_URL= config("DATABASE_URL", default=None)
+if DATABASE_URL is not None:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True
+    )
 }
--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from cfehome.env import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(q3)w+fzzwwc616scd%9q*iuwfnkd6jec8n24y*!51gx*tt4z0'
+SECRET_KEY = config("DJANGO_SECRET_KEY", default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
