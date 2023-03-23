776147d419e73a3a8ae82e4be13051cbcbf6d173
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-23 16:11:01 -0500

    10 - Product List View

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name='products'
 urlpatterns = [
+    path('', views.product_list_view, name='list'),
     path('create/', views.product_create_view, name='create'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -17,3 +17,7 @@ def product_create_view(request):
         form.add_error(None, "Your must be logged in to create products.")
     context['form'] = form
     return render(request, 'products/create.html', context)
+
+def product_list_view(request):
+    object_list = Product.objects.all()
+    return render(request, "products/list.html", {"object_list": object_list})
