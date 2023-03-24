b4617b82f91255ece2924df53e0cc0368789a0c9
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-24 08:59:58 -0500

    14 - Product Attachment Download View

--- src/products/urls.py
+++ src/products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.product_list_view, name='list'),
     path('create/', views.product_create_view, name='create'),
     path('<slug:handle>/', views.product_detail_view, name='detail'),
+    path('<slug:handle>/download/<int:pk>/', views.product_attachment_download_view, name='download'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,11 @@
+import mimetypes
+
+from django.http import FileResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .forms import ProductForm, ProductUpdateForm
-from .models import Product
+from .models import Product, ProductAttachment
 
 
 def product_create_view(request):
@@ -37,3 +40,18 @@ def product_detail_view(request, handle=None):
             # return redirect('/products/create/')
         context['form'] = form
     return render(request, 'products/detail.html', context)
+
+def product_attachment_download_view(request, handle=None, pk=None):
+    attachment = get_object_or_404(ProductAttachment, product__handle=handle, pk=pk)
+    can_download = attachment.is_free or False
+    if request.user.is_authenticated:
+        can_download = True # check ownership
+    if can_download is False:
+        return HttpResponseBadRequest()
+    file = attachment.file.open(mode='rb') # cdn -> S3 object storage
+    filename = attachment.file.name
+    content_type, _ = mimetypes.guess_type(filename)
+    response =  FileResponse(file)
+    response['Content-Type'] = content_type or 'application/octet-stream'
+    response['Content-Disposition'] = f'attachment;filename={filename}'
+    return response
