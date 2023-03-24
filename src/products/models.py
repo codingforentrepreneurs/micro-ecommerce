0044ad793fa4846ddee51cccb5604ee74a7f9288
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-24 15:57:05 -0500

    22 - Integrate Stripe to our Django Models

--- src/products/models.py
+++ src/products/models.py
@@ -1,22 +1,31 @@
 import pathlib
+import stripe
+
+
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.utils import timezone
 from django.urls import reverse
 
+from cfehome.env import config 
+
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default=None)
+stripe.api_key = STRIPE_SECRET_KEY
+
+
 PROTECTED_MEDIA_ROOT = settings.PROTECTED_MEDIA_ROOT
 protected_storage = FileSystemStorage(location=str(PROTECTED_MEDIA_ROOT))
 # Create your models here.
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    # stripe_product_id = 
+    stripe_product_id = models.CharField(max_length=220, blank=True, null=True)
     image = models.ImageField(upload_to="products/", blank=True, null=True)
     name = models.CharField(max_length=120)
     handle = models.SlugField(unique=True) # slug
     price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
     og_price = models.DecimalField(max_digits=10, decimal_places=2, default=9.99)
-    # stripe_price_id = 
+    stripe_price_id = models.CharField(max_length=220, blank=True, null=True)
     stripe_price = models.IntegerField(default=999) # 100 * price
     price_changed_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -34,11 +43,28 @@ class Product(models.Model):
         return self.display_name
 
     def save(self, *args, **kwargs):
+        if self.name:
+            stripe_product_r = stripe.Product.create(name=self.name)
+            self.stripe_product_id = stripe_product_r.id
+        if not self.stripe_price_id:
+            stripe_price_obj = stripe.Price.create(
+                product= self.stripe_product_id,
+                unit_amount=self.stripe_price,
+                currency="usd"
+            )
+            self.stripe_price_id = stripe_price_obj.id
         if self.price != self.og_price:
             # price changed
             self.og_price = self.price
             # trigger an API request for the price
             self.stripe_price = int(self.price * 100)
+            if self.stripe_product_id:
+                stripe_price_obj = stripe.Price.create(
+                    product= self.stripe_product_id,
+                    unit_amount=self.stripe_price,
+                    currency="usd"
+                )
+                self.stripe_price_id = stripe_price_obj.id
             self.price_changed_timestamp = timezone.now()
         super().save(*args, **kwargs)
 
--- src/purchases/views.py
+++ src/purchases/views.py
@@ -1,9 +1,16 @@
 import random
+import stripe
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
-
+from django.urls import reverse
 from products.models import Product
 from .models import Purchase
 
+from cfehome.env import config 
+
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default=None)
+stripe.api_key = STRIPE_SECRET_KEY
+
+BASE_ENDPOINT="http://127.0.0.1:8000"
 
 def purchase_start_view(request):
     if not request.method == "POST":
@@ -12,14 +19,32 @@ def purchase_start_view(request):
         return HttpResponseBadRequest()
     handle = request.POST.get("handle")
     obj = Product.objects.get(handle=handle)
+    stripe_price_id = obj.stripe_price_id
+    if stripe_price_id is None:
+        return HttpResponseBadRequest()
     purchase = Purchase.objects.create(user=request.user, product=obj)
     request.session['purchase_id'] = purchase.id
-    number = random.randint(0, 1)
-    # if number == 1:
-    #     return HttpResponseRedirect("/purchases/success")
-    # stripe api
-    return HttpResponseRedirect("/purchases/stopped")
-    # return HttpResponse("Started")
+    success_path = reverse("purchases:success")
+    if not success_path.startswith("/"):
+        success_path = f"/{success_path}"
+    cancel_path = reverse("purchases:stopped")
+    success_url = f"{BASE_ENDPOINT}{success_path}"
+    cancel_url = f"{BASE_ENDPOINT}{cancel_path}"
+    print(success_url, cancel_url)
+    checkout_session = stripe.checkout.Session.create(
+        line_items = [
+            {
+                "price": stripe_price_id,
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url
+    )
+    purchase.stripe_checkout_session_id = checkout_session.id
+    purchase.save()
+    return HttpResponseRedirect(checkout_session.url)
 
 def purchase_success_view(request):
     purchase_id = request.session.get("purchase_id")
@@ -27,8 +52,16 @@ def purchase_success_view(request):
         purchase = Purchase.objects.get(id=purchase_id)
         purchase.completed = True
         purchase.save()
+        del request.session['purchase_id']
+        return HttpResponseRedirect(purchase.product.get_absolute_url())
     return HttpResponse(f"Finished {purchase_id}")
 
 
 def purchase_stopped_view(request):
+    purchase_id = request.session.get("purchase_id")
+    if purchase_id:
+        purchase = Purchase.objects.get(id=purchase_id)
+        product = purchase.product
+        del request.session['purchase_id']
+        return HttpResponseRedirect(product.get_absolute_url())
     return HttpResponse("Stopped")
