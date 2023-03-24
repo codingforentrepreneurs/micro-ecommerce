29e511e0db9c1f6e08e452dc87e5114d1b3fa1a9
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-24 17:14:29 -0500

    25 - Running with Branched Environments

--- src/cfehome/storages/conf.py
+++ src/cfehome/storages/conf.py
@@ -3,7 +3,7 @@ from cfehome.env import config
 AWS_ACCESS_KEY_ID=config("AWS_ACCESS_KEY_ID", default=None)
 AWS_SECRET_ACCESS_KEY=config("AWS_SECRET_ACCESS_KEY", default=None)
 AWS_S3_SIGNATURE_VERSION="s3v4"
-AWS_STORAGE_BUCKET_NAME = "micro-ecommerce"
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default="micro-ecommerce")
 AWS_S3_ENDPOINT_URL=f"https://{AWS_STORAGE_BUCKET_NAME}.us-east-1.linodeobjects.com"
 
 AWS_DEFAULT_ACL="public-read"
