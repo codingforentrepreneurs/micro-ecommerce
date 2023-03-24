85c07116a69298a59ffab7cbc9e5d4c5091eb141
CodingForEntrepreneurs <hello@teamcfe.com>
2023-03-24 10:02:08 -0500

    15 - Product Attachments Display Table

--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,9 @@
+import pathlib
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 
 PROTECTED_MEDIA_ROOT = settings.PROTECTED_MEDIA_ROOT
 protected_storage = FileSystemStorage(location=str(PROTECTED_MEDIA_ROOT))
@@ -30,7 +32,7 @@ class Product(models.Model):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return f"/products/{self.handle}/"
+        return reverse("products:detail", kwargs={"handle": self.handle})
 
 
 def handle_product_attachment_upload(instance, filename):
@@ -39,7 +41,20 @@ def handle_product_attachment_upload(instance, filename):
 class ProductAttachment(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file = models.FileField(upload_to=handle_product_attachment_upload, storage=protected_storage)
+    name = models.CharField(max_length=120, null=True, blank=True)
     is_free = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = pathlib.Path(self.file.name).name # stem, suffix
+        super().save(*args, **kwargs)
+
+    @property
+    def display_name(self):
+        return self.name or pathlib.Path(self.file.name).name
+    
+    def get_download_url(self):
+        return reverse("products:download", kwargs={"handle": self.product.handle, "pk": self.pk})
--- src/products/views.py
+++ src/products/views.py
@@ -26,19 +26,30 @@ def product_list_view(request):
     return render(request, "products/list.html", {"object_list": object_list})
 
 
-def product_detail_view(request, handle=None):
+
+def product_manage_detail_view(request, handle=None):
     obj = get_object_or_404(Product, handle=handle)
-    is_owner = False
+    is_manager = False
     if request.user.is_authenticated:
-        is_owner = obj.user == request.user
+        is_manager = obj.user == request.user
     context = {"object": obj}
-    if is_owner:
+    if is_manager:
         form = ProductUpdateForm(request.POST or None, request.FILES or None, instance=obj)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.save()
             # return redirect('/products/create/')
         context['form'] = form
+    return render(request, 'products/detail.html', context)
+
+def product_detail_view(request, handle=None):
+    obj = get_object_or_404(Product, handle=handle)
+    attachments = ProductAttachment.objects.filter(product=obj)
+    # attachments = obj.productattachment_set.all()
+    is_owner = False
+    if request.user.is_authenticated:
+        is_owner = True # verify ownership
+    context = {"object": obj, "is_owner": is_owner, "attachments": attachments}
     return render(request, 'products/detail.html', context)
 
 def product_attachment_download_view(request, handle=None, pk=None):
