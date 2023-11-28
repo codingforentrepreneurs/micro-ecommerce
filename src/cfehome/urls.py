856928685391a941e6d79ff3e51e6f681aed2d5f
Justin Mitchel <jmitchel3@gmail.com>
2023-11-28 15:40:33 -0700

    Added login url redirect

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,14 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    path('', views.home_view),
-    path('admin/', admin.site.urls),
-    path('products/',include('products.urls')),
-    path('purchases/',include('purchases.urls')),
+    path("", views.home_view),
+    path("login/", RedirectView.as_view(url="/admin/")),
+    path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
+    path("purchases/", include("purchases.urls")),
 ]
 
 if settings.DEBUG:
